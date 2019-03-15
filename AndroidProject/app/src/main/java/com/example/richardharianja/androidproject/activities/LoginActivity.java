04a2e2ccaac28cf762b7d4343602ca78c95d82a3
richad <richad@doku.com>
2019-03-15 19:19:35 +0700

    Update Login Activity

--- AndroidProject/app/src/main/java/com/example/richardharianja/androidproject/activities/LoginActivity.java
+++ AndroidProject/app/src/main/java/com/example/richardharianja/androidproject/activities/LoginActivity.java
@@ -29,7 +29,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
